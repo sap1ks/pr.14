14684f317fe4c5a585a399d188d71a992ef3c9df
sap1ks <sap1kcsgo@gmail.com>
2024-12-17 09:21:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Вывести на экран целые числа из интервала от 1 до n (цикл с параметром)
+/// </summary>
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Number("Введите число");
+            Func(n);
+           Console.ReadKey();
+        }
+        static int Number(string message)
+        {
+            Console.WriteLine(message);
+            return int.Parse(Console.ReadLine());
+        }
+        static void Func(int n)
+        {
+            for (int x=1;x <= n;x++)
+            {
+                Console.WriteLine(x);
+            }
+        }
+        
+    }
+}
