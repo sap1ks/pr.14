520230ca44e1a19cb13db2bd7ba0437d0ccfb303
sap1ks <sap1kcsgo@gmail.com>
2024-12-17 09:47:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Function();
+            Console.ReadKey();
+        }
+        static void Function()
+        {
+            for(int y=1;y<=25;y++)
+            {
+                Console.WriteLine("*");
+            }
+        }
+        static void Func()
+        {
+            for(int x=1;x<=18;x++)
+            {
+                Console.Write("*");
+            }
+        }
+    }
+}
