da3e49dee69bdbee8fdbf3fec0f42c793c030485
sap1ks <sap1kcsgo@gmail.com>
2024-12-17 09:09:13 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 /// <summary>
 /// Вывести на экран в столбик первые 10 натуральных чисел (циклы с условием, цикл с параметром)
 /// </summary>
+/// Вывести на экран целые числа из интервала от 1 до n (цикл с параметром)
+/// 
 namespace ConsoleApp12
 {
     class Program
@@ -17,12 +19,29 @@ namespace ConsoleApp12
         }
         static void Func()
         {
-            int x = 1;
-            while (x <= 10)
+            for(int x=10;x>=1;x--)
             {
-                Console.WriteLine(x);
-                x++;
+                Console.WriteLine(x);      
             }
         }
+        ////static void Func()
+        ////{
+        ////    int x = 1;
+        ////    do
+        ////    {
+        ////        Console.WriteLine(x);
+        ////        x++;
+        ////    }
+        ////    while (x <= 10);
+        ////}
+        //static void Func()
+        //{
+        //    int x = 1;
+        //    while (x <= 10)
+        //    {
+        //        Console.WriteLine(x);
+        //        x++;
+        //    }
+        //}
     }
 }
